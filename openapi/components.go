bac298bba611cf1ca4694880c2052279719027a0
Alexander Mayers <62880419+alexthemayers@users.noreply.github.com>
2022-12-01 22:25:48 +0200

    add security objects

--- openapi/components.go
+++ openapi/components.go
@@ -26,8 +26,44 @@ type Example struct {
 // Header is a programmatic representation of the Header object defined here:https://swagger.io/specification/#header-object
 type Header struct{}
 
+type OAuthFlows struct {
+	node
+	Implicit          OAuthFlow `yaml:"implicit"`          // Configuration for the OAuth Implicit flow
+	Password          OAuthFlow `yaml:"password"`          // Configuration for the OAuth Resource Owner Password flow
+	ClientCredentials OAuthFlow `yaml:"clientCredentials"` // Configuration for the OAuth Client Credentials flow. Previously called application in OpenAPI 2.0.
+	AuthorizationCode OAuthFlow `yaml:"authorizationCode"` // Configuration for the OAuth Authorization Code flow. Previously called accessCode in OpenAPI 2.0.
+}
+type OAuthFlow struct {
+	node
+	AuthorizationUrl map[string]string `yaml:"authorizationUrl"`
+	TokenUrl         string            `yaml:"tokenUrl"`
+	RefreshUrl       string            `yaml:"refreshUrl"`
+	Scopes           map[string]string `yaml:"scopes"`
+}
+
 // SecurityScheme is a programmatic representation of the SecurityScheme object defined here: https://swagger.io/specification/#security-scheme-object
-type SecurityScheme struct{}
+type SecurityScheme struct {
+	Type        string `yaml:"type"`
+	Description string `yaml:"description"`
+
+	// only applies to API key
+	Name string `yaml:"name"`
+	In   string `yaml:"in"`
+
+	// only applies to http
+	Scheme string `yaml:"scheme"`
+
+	// only applies to http bearer tokens
+	BearerFormat string `yaml:"bearerFormat"`
+
+	// only applies to oauth2
+	Flows OAuthFlows `yaml:"flows"`
+
+	// only applies to openIdConnect
+	OpenIdConnectUrl string `yaml:"openIdConnectUrl"`
+}
+
+type SecurityRequirement map[string][]string // Contains a named set of string arrays eg. { "petstore_auth": ["write:pets", "read:pets"] }
 
 // Link is a programmatic representation of the Link object defined here: https://swagger.io/specification/#link-object
 type Link struct{}
